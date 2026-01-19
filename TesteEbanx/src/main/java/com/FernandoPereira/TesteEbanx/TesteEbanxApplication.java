58091c961c0f6ff2200f5251ce2245144b4e365b
Fernando <fernandopereiraaraujo_01@hotmail.com>
2026-01-19 11:08:41 -0300

    feat:starting project

---
+++ TesteEbanx/src/main/java/com/FernandoPereira/TesteEbanx/TesteEbanxApplication.java
@@ -0,0 +1,13 @@
+package com.FernandoPereira.TesteEbanx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEbanxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteEbanxApplication.class, args);
+	}
+
+}
---
+++ TesteEbanx/src/test/java/com/FernandoPereira/TesteEbanx/TesteEbanxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FernandoPereira.TesteEbanx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEbanxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
